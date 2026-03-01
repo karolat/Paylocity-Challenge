3225421cd0e889dc3839c49e9afb5c0447441aed
karolat <karolat@gmail.com>
2026-03-01 13:08:14 -0500

    initialize playwright in project

---
+++ playwright.config.ts
@@ -0,0 +1,63 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+
+    baseURL: 'https://wmxrwq14uc.execute-api.us-east-1.amazonaws.com/Prod/',
+    extraHTTPHeaders: {
+      Authorization: `Basic ${process.env.AUTH_TOKEN}`,
+    },
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'api',
+      dependencies: ['api-setup'],
+      testMatch: /tests\/api\/.*\.spec\.ts/,
+    },
+    {
+      name: 'chromium',
+      testIgnore: /tests\/api\/.*/,
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      testIgnore: /tests\/api\/.*/,
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      testIgnore: /tests\/api\/.*/,
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
